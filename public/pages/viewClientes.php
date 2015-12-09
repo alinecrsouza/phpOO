51b210cb951f5a424f6896095ae9778af07819d9
Aline da Cruz Rodrigues Souza <linecrsouza@gmail.com>
2015-12-09 11:35:41 -0200

    Corrigindo caminho do rodapé.

--- public/pages/viewClientes.php
+++ public/pages/viewClientes.php
@@ -82,4 +82,4 @@ require_once "header.php";
                             </tbody>
                         </table>
                     </div>
-    <?php require_once "/pages/footer.php"; ?>
+    <?php require_once "/footer.php"; ?>
