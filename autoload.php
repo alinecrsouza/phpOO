990567d8de96d7c4aed1a48dc4db19a2ab05a255
Aline da Cruz Rodrigues Souza <linecrsouza@gmail.com>
2015-12-16 14:42:32 -0200

    Reconfigurando o caminho da constante CLASS_DIR e apagando o conteudo de .htaccess

--- autoload.php
+++ autoload.php
@@ -1,6 +1,8 @@
 <?php
 
-define ('CLASS_DIR', 'src/');
+define ('CLASS_DIR', __DIR__ . "/src/");
 set_include_path(get_include_path().PATH_SEPARATOR.CLASS_DIR);
+//No linux
 //spl_autoload_register(function ($class) { require_once(str_replace('\\', '/', $class . '.php')); });
+//No windows
 spl_autoload_register();
